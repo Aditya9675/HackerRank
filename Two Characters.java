477a8d31c98cc1487e9f4b9807bb603c2d3ce3a6
Aditya Sharma <78315735+Aditya9675@users.noreply.github.com>
2022-08-08 23:38:25 +0530

    Update Two Characters.java

--- Two Characters.java
+++ Two Characters.java
@@ -2,15 +2,17 @@
 
 Given a string, remove characters until the string is made up of any two alternating characters. When you choose a character to remove, all instances of that character must be removed. Determine the longest string possible that contains just two alternating letters.
 
-Example
-
-s = 'abcacdabd'
-
-Delete a, to leave bcdbd. Now, remove the character c to leave the valid string bdbd with a length of 4. Removing either b or d at any point would not result in a valid string. Return 4.
-
-Given a string s, convert it to the longest possible string t made up only of alternating characters. Return the length of string t. If no string t can be formed, return 0.
+**Example88.
 
 
+ 
+$s = \text{abaacdabd}$
+
+Delete a, to leave bcdbd. Now, remove the character c to leave the valid string bdbd with a length of 4. Removing either b or d at any point would not result in a valid string.
+
+Given a string $s$, convert it to the longest possible string $t$ made up only of alternating characters. Print the length of string $t$ on a new line. If no string $t$ can be formed, print $0$ instead.
+
+ 
 
 Function Description
 
@@ -19,24 +21,40 @@ Complete the alternate function in the editor below.
 alternate has the following parameter(s):
 
 string s: a string
-
-
 Returns.
 
-int: the length of the longest valid string, or 0 if there are none
-
+int: the length of the longest valid string, or $0$ if there are none
 Input Format
 
 The first line contains a single integer that denotes the length of s.
-The second line contains string s.
 
+The second line contains string s.
 
 Constraints
 
+1<=lengthofs<=1000
+S[i] belongs to ascii[a-z]
+Sample Input
 
-1  <=  length of s  <=  1000
+10
 
+beabeefeab
+ 
+
+Sample Output
+
+5
+ 
+
+Explanation
+
+The characters present in s are a, b, e, and f. This means that t must consist of two of those characters and we must delete two others. Our choices for characters to leave are [a,b], [a,e], [a, f], [b, e], [b, f] and [e, f].
+
+If we delete e and f, the resulting string is babab. This is a valid t as there are only two distinct characters (a and b), and they are alternating within the string.
+
+If we delete a and f, the resulting string is bebeeeb. This is not a valid string t because there are consecutive e’s present. Removing them would leave consecutive b’s, so this fails to produce a valid string t.
 */
+
 
 import java.io.*;
 import java.math.*;
