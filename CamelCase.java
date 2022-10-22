8faf958a12588c90bdaa2b8dcccc5fdb506a3ec7
Aditya Sharma <78315735+Aditya9675@users.noreply.github.com>
2022-10-23 00:00:19 +0530

    Create CamelCase.java

---
+++ CamelCase.java
@@ -0,0 +1,69 @@
+/*
+
+There is a sequence of words in CamelCase as a string of letters,s, having the following properties:
+
+It is a concatenation of one or more words consisting of English letters.
+All letters in the first word are lowercase.
+For each of the subsequent words, the first letter is uppercase and rest of the letters are lowercase.
+Given s, determine the number of words in s.
+
+Example
+
+s = oneTwoThree
+
+
+There are 3 words in the string: ‘one’, ‘Two’, ‘Three’.
+
+Function Description
+
+Complete the camelcase function in the editor below.
+
+camelcase has the following parameter(s):
+
+string s: the string to analyze
+Returns
+
+int: the number of words in s
+Input Format
+
+A single line containing string s.
+
+Constraints
+
+1<=length of s<=10^5
+Sample Input
+
+saveChangesInTheEditor
+ 
+
+Sample Output
+
+5
+
+*/
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        String p = s.toUpperCase();
+        int l = s.length();
+        int ans = 1;
+        for (int i = 0; i < l; i++) {
+            if (p.charAt(i) == s.charAt(i)) ans++;
+        }
+        
+        System.out.println(ans);
+    }
+}
+
